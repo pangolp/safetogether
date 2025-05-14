6a08ba0bd7084cb2b0b09bed49fa1a2ec9399f16
Pagani Walter <paganiwalter@gmail.com>
2025-05-14 06:37:22 -0300

    Move functions to helpers

--- Contents/mods/safetogether/media/lua/shared/helpers.lua
+++ Contents/mods/safetogether/media/lua/shared/helpers.lua
@@ -34,3 +34,37 @@ function removeItem(item, amount, player)
         end
     end
 end
+
+function getItems(player, item)
+    local _count = 0
+
+    if player then
+        local _inventory = player:getInventory()
+        if _inventory then
+            local _items = _inventory:getItems()
+            if _items then
+                for i = 0, _items:size() - 1 do
+                    local _item = _items:get(i)
+                    if _item:getFullType() == item then
+                        _count = _count + _item:getCount()
+                    end
+                end
+            end
+        end
+    end
+
+    return _count
+end
+
+function getCountSafePerPlayer(player)
+    local _count = 0
+
+    for i = 0, SafeHouse.getSafehouseList():size() - 1 do
+        local _safehouse = SafeHouse.getSafehouseList():get(i)
+        if _safehouse:isOwner(player) then
+            _count = _count + 1
+        end
+    end
+
+    return _count
+end
